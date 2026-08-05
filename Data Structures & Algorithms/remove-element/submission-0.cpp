87acc0591e82e2ec6540c468bded4d40ca9f0e49
byteforge1208 <likhith122001@gmail.com>
2026-08-05 20:26:52 +0530

    Add: remove-element - submission-0

---
+++ Data Structures & Algorithms/remove-element/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void swap(int &a, int &b){
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+
+    int removeElement(vector<int>& nums, int val) {
+        int n = nums.size();
+        int j = n-1;
+        for(int i=n-1; i>=0; i--){
+            if(nums[i] == val){
+                swap(nums[i],nums[j]);
+                j--;
+            }
+        }
+        return j+1;
+        
+    }
+};
